56a537129bc11c12bf8a28e9ccaff1b94cb9afcb
Mônica Alcântara <monicaabrandao88@hotmail.com>
2023-11-12 21:55:55 -0300

    create productService

---
+++ src/product/product.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get } from '@nestjs/common';
+import { Roles } from '../decorators/roles.decorator';
+import { UserType } from '../user/enum/user-type.enum';
+import { ReturnProductDto } from './dtos/return-product.dto';
+import { ProductService } from './product.service';
+
+@Roles(UserType.Admin, UserType.User)
+@Controller('product')
+export class ProductController {
+
+    constructor(
+        private readonly productService: ProductService
+    ) {}
+
+    @Get()
+    async findAll(): Promise<ReturnProductDto[]>{
+        return (await this.productService.findAll())
+        .map((product) => new ReturnProductDto(product));
+    }
+}
