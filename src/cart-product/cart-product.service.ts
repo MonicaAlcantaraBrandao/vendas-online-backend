5903b0a6d863e510a9424a4c3ead95b0dfc6f8c7
Mônica Alcântara <monicaabrandao88@hotmail.com>
2023-11-14 19:23:31 -0300

    create function delete and update cart

--- src/cart-product/cart-product.service.ts
+++ src/cart-product/cart-product.service.ts
@@ -1,10 +1,11 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CartProductEntity } from './entities/cart-product.entity';
-import { Repository } from 'typeorm';
+import { DeleteResult, Repository } from 'typeorm';
 import { InsertCartDto } from '../cart/dtos/insert-cart.dto';
 import { CartEntity } from '../cart/entities/cart.entity';
 import { ProductService } from '../product/product.service';
+import { UpdateCartDto } from 'src/cart/dtos/update-cart.dto';
 
 @Injectable()
 export class CartProductService {
@@ -42,7 +43,9 @@ export class CartProductService {
         cart: CartEntity): Promise<CartProductEntity>{
         await this.productService.findProductById(insertCartDto.productId);
 
-        const cartProduct = await this.verifyProductInCart(insertCartDto.productId, cart.id).catch(() => undefined)
+        const cartProduct = await this.verifyProductInCart(
+            insertCartDto.productId, 
+            cart.id).catch(() => undefined)
 
         if(!cartProduct){
             return this.createProductInCart(insertCartDto, cart.id);
@@ -52,5 +55,24 @@ export class CartProductService {
             ...cartProduct,
             amount: cartProduct.amount + insertCartDto.amount
         });
+    }
+
+    async updateProductInCart(
+        updateCartDto: UpdateCartDto, 
+        cart: CartEntity): Promise<CartProductEntity>{
+        await this.productService.findProductById(updateCartDto.productId);
+
+        const cartProduct = await this.verifyProductInCart(
+            updateCartDto.productId, 
+            cart.id);
+
+        return this.cartProductRepository.save({
+            ...cartProduct,
+            amount: updateCartDto.amount,
+        });
+    }
+
+    async deleteProductCart(productId:number, cartId:number): Promise<DeleteResult>{
+        return this.cartProductRepository.delete({productId, cartId});
     }
 }
