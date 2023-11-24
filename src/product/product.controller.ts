5e70c6144449eec102668ab4dadfe38d2c40b890
Mônica Alcântara <monicaabrandao88@hotmail.com>
2023-11-24 02:21:55 -0300

    create find product page

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,64 +1,86 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, UsePipes, ValidationPipe } from '@nestjs/common';
-import { Roles } from '../decorators/roles.decorator';
-import { UserType } from '../user/enum/user-type.enum';
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Post,
+    Put,
+    Query,
+    UsePipes,
+    ValidationPipe,
+  } from '@nestjs/common';
+  import { DeleteResult } from 'typeorm';
+  import { Roles } from '../decorators/roles.decorator';
+  import { UserType } from '../user/enum/user-type.enum';
+  import { ReturnPriceDeliveryDto } from './dtos/return-price-delivery.dto';
+  import { ProductEntity } from './entities/product.entity';
+  import { ProductService } from './product.service';
 import { ReturnProductDto } from './dtos/return-product.dto';
-import { ProductService } from './product.service';
-import { ProductEntity } from './entities/product.entity';
 import { CreateProductDto } from './dtos/create-product.dto';
-import { DeleteResult } from 'typeorm';
 import { UpdateProductDto } from './dtos/update-product.dto';
-import { ReturnPriceDeliveryDto } from './dtos/return-price-delivery.dto';
-
-@Controller('product')
-export class ProductController {
-
-    constructor(
-        private readonly productService: ProductService
-    ) {}
-
+  
+  @Controller('product')
+  export class ProductController {
+    constructor(private readonly productService: ProductService) {}
+  
     @Roles(UserType.Admin, UserType.Root, UserType.User)
     @Get()
-    async findAll(): Promise<ReturnProductDto[]>{
-        return (await this.productService.findAll([], true))
-        .map((product) => new ReturnProductDto(product));
+    async findAll(): Promise<ReturnProductDto[]> {
+      return (await this.productService.findAll([], true)).map(
+        (product) => new ReturnProductDto(product),
+      );
     }
-
+  
+    @Roles(UserType.Admin, UserType.Root, UserType.User)
+    @Get('/page')
+    async findAllPage(@Query('search') search: string): Promise<ReturnProductDto[]> {
+      return (await this.productService.findAllPage(search)).map(
+        (product) => new ReturnProductDto(product),
+      );
+    }
+  
     @Roles(UserType.Admin, UserType.Root, UserType.User)
     @Get('/:productId')
-    async findProductById(@Param('productId') productId: number): Promise<ReturnProductDto>{
-        return new ReturnProductDto (await this.productService.findProductById(productId, true));
+    async findProductById(@Param('productId') productId): Promise<ReturnProductDto> {
+      return new ReturnProductDto(
+        await this.productService.findProductById(productId, true),
+      );
     }
-
+  
     @Roles(UserType.Admin, UserType.Root)
     @UsePipes(ValidationPipe)
     @Post()
-    async createProduct(@Body() createProduct: CreateProductDto): Promise<ProductEntity> {
-        return this.productService.createProduct(createProduct);
+    async createProduct(
+      @Body() createProduct: CreateProductDto,
+    ): Promise<ProductEntity> {
+      return this.productService.createProduct(createProduct);
     }
-
+  
     @Roles(UserType.Admin, UserType.Root)
     @Delete('/:productId')
     async deleteProduct(
-        @Param('productId') productId: number
+      @Param('productId') productId: number,
     ): Promise<DeleteResult> {
-        return this.productService.deleteProduct(productId);
+      return this.productService.deleteProduct(productId);
     }
-
+  
     @Roles(UserType.Admin, UserType.Root)
     @UsePipes(ValidationPipe)
     @Put('/:productId')
     async updateProduct(
-        @Body() updateProduct: UpdateProductDto,
-        @Param('productId') productId: number
+      @Body() updateProduct: UpdateProductDto,
+      @Param('productId') productId: number,
     ): Promise<ProductEntity> {
-        return this.productService.updateProduct(updateProduct, productId);
+      return this.productService.updateProduct(updateProduct, productId);
     }
-
-    @Get('/:productId/delivery/:cep')
+  
+    @Get('/:idProduct/delivery/:cep')
     async findPriceDelivery(
-        @Param('idProduct') idProduct: number, 
-        @Param('cep') cep:string
-        ): Promise<ReturnPriceDeliveryDto>{
-        return this.productService.findPriceDelivery(cep, idProduct);
+      @Param('idProduct') idProduct: number,
+      @Param('cep') cep: string,
+    ): Promise<ReturnPriceDeliveryDto> {
+      return this.productService.findPriceDelivery(cep, idProduct);
     }
-}
+  }
+  
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,137 +1,165 @@
-import { Injectable, NotFoundException, forwardRef, Inject, BadRequestException } from '@nestjs/common';
+import {
+    BadRequestException,
+    forwardRef,
+    Inject,
+    Injectable,
+    NotFoundException,
+  } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { ProductEntity } from './entities/product.entity';
-import { DeleteResult, In, Repository } from 'typeorm';
-import { CreateProductDto } from './dtos/create-product.dto';
 import { CategoryService } from '../category/category.service';
-import { UpdateProductDto } from './dtos/update-product.dto';
+import { DeleteResult, ILike, In, Repository } from 'typeorm';
+import { ProductEntity } from './entities/product.entity';
 import { CountProduct } from './dtos/count-product.dto';
-import { SizeProductDto } from '../correios/dtos/size-product.dto';
 import { CorreiosService } from '../correios/correios.service';
 import { CdServiceEnum } from '../correios/enums/cd-service.enum';
 import { ReturnPriceDeliveryDto } from './dtos/return-price-delivery.dto';
-
-@Injectable()
-export class ProductService {
+import { CreateProductDto } from './dtos/create-product.dto';
+import { UpdateProductDto } from './dtos/update-product.dto';
+import { SizeProductDto } from 'src/correios/dtos/size-product.dto';
+  
+  @Injectable()
+  export class ProductService {
     constructor(
-        @InjectRepository(ProductEntity)
-        private readonly productRepository: Repository<ProductEntity>,
-
-        @Inject(forwardRef(() => CategoryService))
-        private readonly categoryService: CategoryService,
-
-        private readonly correiosService: CorreiosService,
-    ){}
-
-    async findAll(productId?: number[], isFindRelations?: boolean): Promise<ProductEntity[]>{
-        let findOptions = {};
-
-        if (productId && productId.length > 0) {
-            findOptions = {
-                where:{
-                    id: In(productId)
-                },
-            };
-        }
-
-        if(isFindRelations) {
-            findOptions = {
-                ...findOptions,
-                relations:{
-                    category: true,
-                },
-            };
-        }
-
-        const products = await this.productRepository.find(findOptions);
-
-        if (!products || products.length === 0){
-           throw new NotFoundException('Not found products')
-        }
-
-        return products;
+      @InjectRepository(ProductEntity)
+      private readonly productRepository: Repository<ProductEntity>,
+  
+      @Inject(forwardRef(() => CategoryService))
+      private readonly categoryService: CategoryService,
+  
+      private readonly correiosService: CorreiosService,
+    ) {}
+  
+    async findAllPage(search?: string): Promise<ProductEntity[]> {
+      let findOptions = {};
+      if (search) {
+        findOptions = {
+          where: {
+            name: ILike(`%${search}%`),
+          },
+        };
+      }
+      const products = await this.productRepository.find(findOptions);
+  
+      if (!products || products.length === 0) {
+        throw new NotFoundException('Not found products');
+      }
+  
+      return products;
     }
-
-    async createProduct(createProduct:CreateProductDto) :Promise<ProductEntity>{
-        await this.categoryService.findCategoryById(createProduct.categoryId);
-
-        return this.productRepository.save({
-            ...createProduct,
-            weight: createProduct.weight || 0,
-            width: createProduct.width || 0,
-            length: createProduct.length || 0,
-            height: createProduct.height || 0,
-            diameter: createProduct.diameter || 0,
-        })
+  
+    async findAll(
+      productId?: number[],
+      isFindRelations?: boolean,
+    ): Promise<ProductEntity[]> {
+      let findOptions = {};
+  
+      if (productId && productId.length > 0) {
+        findOptions = {
+          where: {
+            id: In(productId),
+          },
+        };
+      }
+  
+      if (isFindRelations) {
+        findOptions = {
+          ...findOptions,
+          relations: {
+            category: true,
+          },
+        };
+      }
+  
+      const products = await this.productRepository.find(findOptions);
+  
+      if (!products || products.length === 0) {
+        throw new NotFoundException('Not found products');
+      }
+  
+      return products;
     }
-
-    async findProductById(productId: number, isRelations?: boolean): Promise<ProductEntity> {
-        const relations = isRelations 
+  
+    async createProduct(createProduct: CreateProductDto): Promise<ProductEntity> {
+      await this.categoryService.findCategoryById(createProduct.categoryId);
+  
+      return this.productRepository.save({
+        ...createProduct,
+        weight: createProduct.weight || 0,
+        width: createProduct.width || 0,
+        length: createProduct.length || 0,
+        diameter: createProduct.diameter || 0,
+        height: createProduct.height || 0,
+      });
+    }
+  
+    async findProductById(
+      productId: number,
+      isRelations?: boolean,
+    ): Promise<ProductEntity> {
+      const relations = isRelations
         ? {
             category: true,
-        } 
+          }
         : undefined;
-
-        const product = await this.productRepository.findOne({
-            where:{
-                id: productId
-            },
-            relations,
-        });
-
-        if(!product) {
-            throw new NotFoundException(`Product id '${productId}' not found`)
-        }
-        return product;
+  
+      const product = await this.productRepository.findOne({
+        where: {
+          id: productId,
+        },
+        relations,
+      });
+  
+      if (!product) {
+        throw new NotFoundException(`Product id: ${productId} not found`);
+      }
+  
+      return product;
     }
-
-    async deleteProduct(productId: number): Promise<DeleteResult>{
-        await this.findProductById(productId);
-
-        return this.productRepository.delete({id: productId});
+  
+    async deleteProduct(productId: number): Promise<DeleteResult> {
+      await this.findProductById(productId);
+  
+      return this.productRepository.delete({ id: productId });
     }
-
-    async updateProduct(updateProduct: UpdateProductDto, productId: number): Promise<ProductEntity>{
-        const product = await this.findProductById(productId);
-
-        return this.productRepository.save({
-            ...product,
-            ...updateProduct,
-        });
+  
+    async updateProduct(
+      updateProduct: UpdateProductDto,
+      productId: number,
+    ): Promise<ProductEntity> {
+      const product = await this.findProductById(productId);
+  
+      return this.productRepository.save({
+        ...product,
+        ...updateProduct,
+      });
     }
-
-    async countProdutsByCategoryId(): Promise<CountProduct[]> { 
-        return this.productRepository.createQueryBuilder('product')
+  
+    async countProdutsByCategoryId(): Promise<CountProduct[]> {
+      return this.productRepository
+        .createQueryBuilder('product')
         .select('product.category_id, COUNT(*) as total')
         .groupBy('product.category_id')
         .getRawMany();
     }
-
+  
     async findPriceDelivery(cep: string, idProduct: number): Promise<any> {
-        const product = await this.findProductById(idProduct);
-
-        const sizeProduct = new SizeProductDto(product);
-
-        const resultPrice = await Promise.all([
-            this.correiosService.priceDelivery(
-                CdServiceEnum.PAC, 
-                cep, 
-                sizeProduct
-            ),
-            this.correiosService.priceDelivery(
-                CdServiceEnum.SEDEX, 
-                cep, 
-                sizeProduct
-            ),
-            this.correiosService.priceDelivery(
-                CdServiceEnum.SEDEX_10, 
-                cep, 
-                sizeProduct
-            ),
-        ]).catch(() => {
-            throw new BadRequestException('Error find delivery price')
-        })
-        
-        return new ReturnPriceDeliveryDto(resultPrice);
+      const product = await this.findProductById(idProduct);
+  
+      const sizeProduct = new SizeProductDto(product);
+  
+      const resultPrice = await Promise.all([
+        this.correiosService.priceDelivery(CdServiceEnum.PAC, cep, sizeProduct),
+        this.correiosService.priceDelivery(CdServiceEnum.SEDEX, cep, sizeProduct),
+        this.correiosService.priceDelivery(
+          CdServiceEnum.SEDEX_10,
+          cep,
+          sizeProduct,
+        ),
+      ]).catch(() => {
+        throw new BadRequestException('Error find delivery price');
+      });
+  
+      return new ReturnPriceDeliveryDto(resultPrice);
     }
-}
+  }
+  
